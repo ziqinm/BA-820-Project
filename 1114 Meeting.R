0f0e4206ee6a0532ed63e4776105a8150cd421e8
Ziqin Ma <ziqinm1279@gmail.com>
2019-11-15 16:19:50 -0500

    .

--- 1114 Meeting.R
+++ 1114 Meeting.R
@@ -46,12 +46,41 @@ glimpse(textCol)
 ## Can be factored, then used in SML methods
 # LotShape: Reg = 4, IR1 = 3, IR2 = 2, IR1 = 1
 textCol$LotShape = as.numeric(factor(textCol$LotShape))
+# LandDlope: Gtl = 1, Mod = 2, Sev = 3
+textCol$LandSlope = as.numeric(factor(textCol$LandSlope))
+# ExterQual: Ex = 1, Gd = 3, TA = 4, Fa = 2
+textCol$ExterQual = as.numeric(factor(textCol$ExterQual))
+# ExterCond: TA = 5, Gd = 3, Fa = 2, Po = 4, Ex = 1
+textCol$ExterCond = as.numeric(factor(textCol$ExterCond))
+# BsmtQual: Ex = 1, TA = 4, Gd = 3, FA = 2, NA = 0
+textCol$BsmtQual = as.numeric(factor(textCol$BsmtQual))
+replace_na(textCol$BsmtQual, 0)
+# BsmtCond: TA = 4, Gd = 2, FA = 1, Po = 3, NA = 0
+textCol$BsmtCond = as.numeric(factor(textCol$BsmtCond))
+replace_na(textCol$BsmtCond, 0)
+# BsmtExposure
+# HeatingQC
+# CentralAir
+# KitchenQual
+# FireplaceQu
+# GarageQual
+# GarageCond
+
+
 
 ## Cannot/have NA in test set(cheating this way) => EFA only
 # Utilities: AllPub = 4, NoSewr = 3, NoSeWa = 2, ELO = 1
 unique(textCol$Utilities)
 textCol$Utilities = gsub("AllPub", 4, textCol$Utilities)
 textCol$Utilities = gsub("NoSeWa", 2, textCol$Utilities)
+# BsmtFinType1
+# BsmtFinType2
+# Electrical
+# Functional
+# GarageFinish
+# PavedFrive
+# PoolQC
+
 
            
 # Extraxt other numeric scale variables
@@ -60,6 +89,9 @@ scaleName = c("OverallQual", "OverallCond")
 scaleCol = train %>% select(scaleName)
 glimpse(scaleCol)
 train %>% select(-textName, -scaleName) %>% glimpse() -> train_num
+
+
+
 
 # Techniques:
 #   CLustering: HClust(numeric + char), KMeans(numeric)
